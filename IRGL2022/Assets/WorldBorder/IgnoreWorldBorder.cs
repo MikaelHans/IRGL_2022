b21b339ba092746fa8d4c5b37d6c5ba6ee2fedf1
kaburov38 <66990962+kaburov38@users.noreply.github.com>
2022-01-04 14:13:51 +0700

    fix world border + drop gun on death

--- IRGL2022/Assets/WorldBorder/IgnoreWorldBorder.cs
+++ IRGL2022/Assets/WorldBorder/IgnoreWorldBorder.cs
@@ -6,9 +6,20 @@ public class IgnoreWorldBorder : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject worldborder;
-    void Start()
+    void Awake()
     {
-        Physics.IgnoreCollision(worldborder.GetComponentInChildren<CapsuleCollider>(), GetComponent<Collider>());
-        Physics.IgnoreCollision(worldborder.GetComponent<MeshCollider>(), GetComponent<Collider>());
+        worldborder = GameObject.FindGameObjectWithTag("WorldBorder");
+               
+    }
+
+    private void Start()
+    {
+
+        if (worldborder != null)
+        {
+            Debug.Log("ignore collider");
+            //Physics.IgnoreCollision(worldborder.GetComponentInChildren<CapsuleCollider>(), GetComponent<Collider>());
+            Physics.IgnoreCollision(worldborder.GetComponent<MeshCollider>(), GetComponent<Collider>());
+        }
     }
 }
