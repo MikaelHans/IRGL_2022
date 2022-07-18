6b461331988e0fe5083cac2e998c9f8e79a9723f
MikaelHans <73513335+MikaelHans@users.noreply.github.com>
2022-07-18 22:46:19 +0700

    dropsystem implemented

--- IRGL2022/Assets/Drop.cs
+++ IRGL2022/Assets/Drop.cs
@@ -6,11 +6,16 @@ using Photon.Pun;
 public class Drop : MonoBehaviourPun
 {
     public Transform dropspot;
+    public float dropForce;
     public void DropItem(ItemData item)
     {
         string itemname = item.ItemName;
 
-        GameObject drop = PhotonNetwork.Instantiate(itemname,dropspot.position, dropspot.rotation);
+        GameObject drop = PhotonNetwork.Instantiate("Prefabs/"+itemname,dropspot.position, dropspot.rotation);
+
+        Vector3 force = new Vector3(0, dropForce, dropForce);
+
+        drop.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * dropForce);
     }
 
 
